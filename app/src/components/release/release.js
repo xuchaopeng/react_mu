50334d1a0fc80775a1479743f2550eb947c47ff6
xcp <m18770074875_1@163.com>
2018-07-17 20:18:35 +0800

    增加提交组件

---
+++ app/src/components/release/release.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+import classnames from 'classnames';
+
+class Release extends Component{
+	constructor(){
+		super();
+	}
+
+	render(){
+		const classstr = classnames({
+
+		});
+
+		return (
+			<div className={classstr}>
+				<textarea name="" id="" cols="30" rows="10">
+					
+				</textarea>
+			</div>
+		)
+	}
+}
+
+export default Release ; 
